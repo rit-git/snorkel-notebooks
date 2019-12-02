5dcdeb309fd74a4fbc0b430b98d39e3138c2f14d
Sara Evensen <sara.evensen@gmail.com>
2019-12-02 13:33:54 -0800

    Add jupyter notebook to streamline writing snorkel functions for spam classification

---
+++ analyzer.py
@@ -0,0 +1,67 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from snorkel.analysis import metric_score
+from snorkel.labeling import filter_unlabeled_dataframe
+from snorkel.utils import preds_to_probs
+import tensorflow as tf
+
+
+def get_keras_logreg(input_dim, output_dim=2):
+    model = tf.keras.Sequential()
+    if output_dim == 1:
+        loss = "binary_crossentropy"
+        activation = tf.nn.sigmoid
+    else:
+        loss = "categorical_crossentropy"
+        activation = tf.nn.softmax
+    dense = tf.keras.layers.Dense(
+        units=output_dim,
+        input_dim=input_dim,
+        activation=activation,
+        kernel_regularizer=tf.keras.regularizers.l2(0.001),
+    )
+    model.add(dense)
+    opt = tf.keras.optimizers.Adam(lr=0.01)
+    model.compile(optimizer=opt, loss=loss, metrics=["accuracy"])
+    return model
+
+def get_keras_early_stopping(patience=10, monitor="val_accuracy"):
+    """Stops training if monitor value doesn't exceed the current max value after patience num of epochs"""
+    return tf.keras.callbacks.EarlyStopping(
+        monitor=monitor, patience=patience, verbose=1, restore_best_weights=True
+    )
+
+
+def train_model(label_model, df_train, df_valid, df_test, L_train):
+    # Train a model on top
+
+    probs_train = label_model.predict_proba(L=L_train)
+
+    df_train_filtered, probs_train_filtered = filter_unlabeled_dataframe(
+        X=df_train, y=probs_train, L=L_train
+    )
+
+    vectorizer = CountVectorizer(ngram_range=(1, 2))
+    X_train = vectorizer.fit_transform(df_train_filtered.text.tolist())
+
+    X_valid = vectorizer.transform(df_valid.text.tolist())
+    X_test = vectorizer.transform(df_test.text.tolist())
+
+    Y_train = df_train.label.values
+    Y_valid = df_valid.label.values
+    Y_test = df_test.label.values
+
+    # Define a vanilla logistic regression model with Keras
+    keras_model = get_keras_logreg(input_dim=X_train.shape[1])
+
+    keras_model.fit(
+        x=X_train,
+        y=probs_train_filtered,
+        validation_data=(X_valid, preds_to_probs(Y_valid, 2)),
+        callbacks=[get_keras_early_stopping()],
+        epochs=50,
+        verbose=0,
+    )
+
+    preds_test = keras_model.predict(x=X_test).argmax(axis=1)
+    test_acc = metric_score(golds=Y_test, preds=preds_test, metric="accuracy")
+    print(f"Test Accuracy: {test_acc * 100:.1f}%")
---
+++ data/preparer.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import glob
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def load_youtube_dataset(load_train_labels: bool = False, split_dev: bool = True, delimiter: str=None):
+    filenames = sorted(glob.glob("data/Youtube*.csv"))
+
+    dfs = []
+    for i, filename in enumerate(filenames, start=1):
+        df = pd.read_csv(filename)
+        # Lowercase column names
+        df.columns = map(str.lower, df.columns)
+        # Remove comment_id field
+        df = df.drop("comment_id", axis=1)
+        # Add field indicating source video
+        df["video"] = [i] * len(df)
+        # Rename fields
+        df = df.rename(columns={"class": "label", "content": "text"})
+        # Remove delimiter chars
+        df['text'].replace(regex=True, inplace=True, to_replace=delimiter, value=r'')
+        # Shuffle order
+        df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+        dfs.append(df)
+
+    df_train = pd.concat(dfs[:4]).sample(800, random_state=123)
+    if split_dev:
+        df_dev = df_train.sample(100, random_state=123)
+
+    if not load_train_labels:
+        df_train["label"] = np.ones(len(df_train["label"])) * -1
+    df_valid_test = dfs[4]
+    df_valid, df_test = train_test_split(
+        df_valid_test, test_size=250, random_state=123, stratify=df_valid_test.label
+    )
+
+    if split_dev:
+        return df_train, df_dev, df_valid, df_test
+    else:
+        return df_train, df_valid, df_test
+
+
+def load_amazon_dataset(load_train_labels: bool = False, split_dev: bool = True, delimiter: str=None):
+    filenames = sorted(glob.glob("data/Amazon*.csv"))
+
+    dfs = []
+    for i, filename in enumerate(filenames, start=1):
+        df = pd.read_csv(filename, header=None)
+        # Lowercase column names
+        df.columns = ["key", "text", "label"]
+        # Remove delimiter chars
+        if delimiter:
+            df['text'].replace(regex=True, inplace=True, to_replace=delimiter, value=r'')
+        # Shuffle order
+        df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+        dfs.append(df)
+
+    df_train = dfs[1]
+    if split_dev:
+        df_dev = df_train.sample(100, random_state=123)
+
+    if not load_train_labels:
+        df_train["label"] = np.ones(len(df_train["label"])) * -1
+    df_valid_test = dfs[0]
+    df_valid, df_test = train_test_split(
+        df_valid_test, test_size=250, random_state=123, stratify=df_valid_test.label
+    )
+
+    if split_dev:
+        return df_train, df_dev, df_valid, df_test
+    else:
+        return df_train, df_valid, df_test
+
+if __name__=="__main__":
+    df_train, df_dev, df_valid, df_test = load_amazon_dataset()
