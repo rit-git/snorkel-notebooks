f8e6633753c9ff744271161e2af671f15ad90ca9
Sara Evensen <sara.evensen@gmail.com>
2019-12-05 13:25:21 -0800

    remove all except core babble functionality

--- babble/__init__.py
+++ babble/__init__.py
@@ -3,3 +3,4 @@ from .parsing import Rule, Grammar, Parse, SemanticParser
 from .filter_bank import FilterBank
 from .utils import ExplanationIO, link_explanation_candidates
 from .babbler import Babbler, BabbleStream
+from .Candidate import Candidate
